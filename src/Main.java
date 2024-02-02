4dc78754c4cbeb80fd39ef537cdc7e70439e3406
ZN-Zz <badhikari1211@gmail.com>
2024-02-02 09:08:47 -0500

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
 
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        double SHIPPING_FEE = 0.02;
+        double freeShipping = 100;
+        double finalPrice;
+        double shippingCost;
+
+        System.out.print("Enter the price of the item: $");
+        double itemPrice = in.nextDouble();
+
+        if (itemPrice >= freeShipping)
+        {
+            shippingCost = 0;
+
+        }
+        else
+        {
+            shippingCost = itemPrice * SHIPPING_FEE;
+        }
+
+        finalPrice = shippingCost + itemPrice;
+        System.out.println("The Shipping Cost is: $" + shippingCost);
+        System.out.println("The Final Price is: $" + finalPrice);
+
     }
 }
